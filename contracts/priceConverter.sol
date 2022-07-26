c67bc0415417d85ee00f82f32d760c6f84b3cf80
pardeep889 <pardeep889@hotmail.com>
2022-07-26 16:40:22 +0530

    added more power to smart contracts and localnetworks

--- contracts/priceConverter.sol
+++ contracts/priceConverter.sol
@@ -1,18 +1,30 @@
-// SPDX-License-Identifier: MIT 
-pragma solidity ^0.8.8;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 library PriceConverter {
-    function getPrice() public view returns(uint256){
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
-        // (uint80 roundId, int price, uint startedAt, uint timestamp, uint80 answeredInRound) = priceFeed.latestRoundData()
-        (,int256 price,,,) = priceFeed.latestRoundData();
-        return uint256(price * 1e10);
-    }
+  function getPrice(AggregatorV3Interface priceFeed)
+    internal
+    view
+    returns (uint256)
+  {
+    (, int256 answer, , , ) = priceFeed.latestRoundData();
+    // ETH/USD rate in 18 digit
+    return uint256(answer * 10000000000);
+  }
 
-    function getConversionRate(uint256 ethAmount) public view returns(uint256){
-        uint256 ethPrice = getPrice();
-        uint256 ethAmountInUSD = (ethPrice * ethAmount) / 1e18;
-        return ethAmountInUSD;
-    }
+  // 1000000000
+  // call it get fiatConversionRate, since it assumes something about decimals
+  // It wouldn't work for every aggregator
+  function getConversionRate(uint256 ethAmount, AggregatorV3Interface priceFeed)
+    internal
+    view
+    returns (uint256)
+  {
+    uint256 ethPrice = getPrice(priceFeed);
+    uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1000000000000000000;
+    // the actual ETH/USD conversation rate, after adjusting the extra 0s.
+    return ethAmountInUsd;
+  }
 }
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,46 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-etherscan")
+require("dotenv").config();
+require("hardhat-gas-reporter")
+require("solidity-coverage")
+require("hardhat-deploy")
+
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.9",
+  defaultNetwork: "hardhat",
+  networks: {
+    rinkeby:{
+      url: process.env.RPC_URL,
+      accounts: [process.env.PRIVATE_KEY],
+      chainId: 4
+    },
+    localhost: {
+      url: "http://127.0.0.1:8545/",
+      chainId: 31337
+    }
+  },
+  solidity: {
+    compilers: [
+      {version: "0.8.9"},
+      {version: "0.6.6"}
+    ]
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY
+  },
+  gasReporter: {
+    enabled: true,
+    outputFile: "gas-report.txt",
+    noColors: true,
+    currency: "USD",
+    coinmarketcap: process.env.COINMARKETCAP_API_KEY,
+    token: "MATIC"
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0,
+      1:0
+    }
+  }
 };
